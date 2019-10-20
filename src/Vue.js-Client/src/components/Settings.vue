5f7936b09ee44af7d97fe79fcc82e97c0c8dbac4
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2019-10-20 21:55:14 +0200

    SettingsView: you can now edit all settings, but the inputs are not validated

--- src/Vue.js-Client/src/components/Settings.vue
+++ src/Vue.js-Client/src/components/Settings.vue
@@ -2,29 +2,30 @@
   <div class="container">
     <div class="row mt-2">
       <div class="col-12 col-md-8 offset-md-2 ">
-        
+        <div class="alert alert-danger text-center" role="alert">
+          Die Eingaben werden aktuell nicht validiert! Ändere was nur, wenn du weißt was du tuest! <br>
+          Um die Werte zu speichern, Enter drücken.
+        </div>
         <div v-if="settings.length!==0" class="table-responsive">
           <table class="table">
             <thead>
               <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Description</th>
-                <th scope="col">Value</th>
+                <th scope="col" class="col-4">Name</th>
+                <th scope="col" class="col-4">Description</th>
+                <th scope="col" class="col-4">Value</th>
               </tr>
             </thead>
             <tbody>
               <tr v-for="s in settings" :key="s.name">
                 <td>{{s.name}}</td>
                 <td>{{s.description}}</td>
-                <template v-if="s.name === 'defaultNumberOfPersonsToClean'">
-                  <input 
-                  type="number"
-                  min="0"
+                <td >
+                  <input
                   class="form-control"
                   :value="s.value"
                   @change="updateSettings(s.name,$event.target.value)"
                   >
-                </template>
+                </td>
               </tr>
             </tbody>
           </table>
