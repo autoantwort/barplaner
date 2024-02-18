9075ea31af91889c992be81e9b2f6dd62a12f755
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-02-18 13:24:26 +0100

    Item Image Model: Use dynamic viewport height

--- src/Vue.js-Client/src/components/stock/ItemList.vue
+++ src/Vue.js-Client/src/components/stock/ItemList.vue
@@ -94,7 +94,7 @@
       <div v-if="loading === true" class="justify-content-center" style="display: flex">
         <b-spinner class="center" label="Loading..."></b-spinner>
       </div>
-      <img v-if="selectedImageId" style="width: 100%; height: 80vh; object-fit: contain;" :src="baseURL + selectedImageId" v-on:load="loading = false" />
+      <img v-if="selectedImageId" style="width: 100%; height: 80dvh; object-fit: contain;" :src="baseURL + selectedImageId" v-on:load="loading = false" />
     </b-modal>
   </div>
 </template>
