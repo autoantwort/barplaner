0cfd8a5ca1c1386fcbf3fe3080fc801454659406
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-03-14 12:04:26 +0100

    Frontend: Phonetics filter: Sort exact matches to the start. Search only at the start of words

--- src/Vue.js-Client/src/phoneticsFilter.js
+++ src/Vue.js-Client/src/phoneticsFilter.js
@@ -2,16 +2,22 @@ import phonetics from "../../Nodejs-RestAPIs/app/util/colognePhonetics";
 
 export default (array, searchedValue, additionalFilter) => {
     const otherFilter = additionalFilter ? additionalFilter : () => false;
-    const v = phonetics.convert(searchedValue).split(" ");
-    return array.filter(p => {
+    const v = phonetics.convert(searchedValue).split(" ").map(s => new RegExp(`(^| )${s}`));
+    const r = new RegExp(searchedValue, "i");
+    const exactMatches = [];
+    return exactMatches.concat(array.filter(p => {
         const resultColognePhonetics = () => {
+            if (p.name.search(r) !== -1) {
+                exactMatches.push(p);
+                return false;
+            }
             for (let s of v) {
-                if (p.nameColognePhonetics.indexOf(s) === -1) {
+                if (p.nameColognePhonetics.search(s) === -1) {
                     return false;
                 }
             }
             return true;
         }
         return resultColognePhonetics() || otherFilter(p);
-    });
+    }));
 };
