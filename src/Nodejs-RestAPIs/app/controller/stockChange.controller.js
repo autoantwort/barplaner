autoantwort/barplaner

commit 1383ae73b9bf41291ded5dcc1a9ee76101f3ff5a

Backend: Item Stock: Also include items without stock changes Frontend: Adapt to this change

src/Nodejs-RestAPIs/app/controller/stockChange.controller.js
@@ -87,30 +87,25 @@ exports.getStockChanges = (req, res) => {
     });
 };
 
-const additionalAttributes = {
-    attributes: {
-        include: [
-            [sequelize.fn('SUM', sequelize.col('stockChange.amount')), "inStock"],
+// get current Stock
+exports.getItemStock = (req, res) => {
+    Item.findAll({
+        attributes: [
+            "name",
+            "nameColognePhonetics",
+            [sequelize.fn('COALESCE', sequelize.fn('SUM', sequelize.col('stockChanges.amount')), 0), "inStock"],
             [sequelize.fn('ROUND', sequelize.fn('MIN', sequelize.col('brottoPrice')), 2), "minBrottoPrice"],
             [sequelize.fn('ROUND', sequelize.fn('MAX', sequelize.col('brottoPrice')), 2), "maxBrottoPrice"],
             [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('brottoPrice')), 2), "avgBrottoPrice"],
         ],
-    },
-};
-
-// get current Stock
-exports.getItemStock = (req, res) => {
-    StockChange.findAll({
-        ...additionalAttributes,
-        group: "itemId",
         include: [{
-            attributes: ["name", "nameColognePhonetics"],
-            model: Item,
-            include: [{
-                attributes: ["id", "name"],
-                model: ItemGroup,
-            }],
+            model: StockChange,
+            attributes: [],
+        }, {
+            model: ItemGroup,
+            attributes: ["id", "name"],
         }],
+        group: "itemId",
     }).then(stock => {
         res.send(stock);
     }).catch(err => {
@@ -119,18 +114,20 @@ exports.getItemStock = (req, res) => {
 };
 
 exports.getStockForItem = (req, res) => {
-    StockChange.findAll({
+    Item.findAll({
         where: {
-            itemId: req.params.itemId,
+            id: req.params.itemId,
         },
-        ...additionalAttributes,
+        attributes: [],
+        include: [{
+            model: StockChange,
+            attributes: [
+                [sequelize.fn('COALESCE', sequelize.fn('SUM', sequelize.col('stockChanges.amount')), 0), "inStock"]
+            ],
+        }],
         group: "itemId",
     }).then(stock => {
-        if (stock.length === 0) {
-            res.send({ inStock: 0 });
-        } else {
-            res.send(stock[0]);
-        }
+        res.send(stock[0].stockChanges[0]);
     }).catch(err => {
         res.status(500).send("Error -> " + err);
     });

src/Vue.js-Client/src/components/stock/ItemStockList.vue
@@ -24,15 +24,15 @@
               <tbody>
                 <tr v-for="item in filteredItems" :key="item.id">
                   <td>
-                    <router-link :to="{ name: 'item', params: { itemId: item.itemId } }">{{
-                      item.stockItem.name
+                    <router-link :to="{ name: 'item', params: { itemId: item.id } }">{{
+                      item.name
                     }}</router-link>
                   </td>
                   <td>
                     <router-link
-                      v-if="item.stockItem.itemGroup"
-                      :to="{ name: 'itemGroup', params: { itemGroupId: item.stockItem.itemGroup.id } }"
-                      >{{ item.stockItem.itemGroup.name }}</router-link
+                      v-if="item.itemGroup"
+                      :to="{ name: 'itemGroup', params: { itemGroupId: item.itemGroup.id } }"
+                      >{{ item.itemGroup.name }}</router-link
                     >
                   </td>
                   <td>{{ item.inStock }}</td>
@@ -79,9 +79,6 @@ export default {
         .get("/itemStock")
         .then((response) => {
           this.filteredItems = this.items = response.data;
-          for (let item of this.items) {
-            item.nameColognePhonetics = item.stockItem.nameColognePhonetics;
-          }
         })
         .catch((e) => {
           console.error(e);

commit 94baa151dbcbb2125c6db207e81dde3a28c9d605

Backend: ItemStock: Return value even if there is no stock change. Regression introduced in 1383ae73b9bf41291ded5dcc1a9ee76101f3ff5a

src/Nodejs-RestAPIs/app/controller/stockChange.controller.js
@@ -128,7 +128,7 @@ exports.getStockForItem = (req, res) => {
         }],
         group: "itemId",
     }).then(stock => {
-        res.send(stock[0].stockChanges[0]);
+        res.send(stock[0].stockChanges[0] ?? {inStock: 0});
     }).catch(err => {
         res.status(500).send("Error -> " + err);
     });