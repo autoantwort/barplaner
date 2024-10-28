a22535b4c020482950cd12bbeaf523fac32b8b9d
autoantwort <Leander.schulten@rwth-aachen.de>
2024-10-28 16:00:23 +0100

    Format

--- src/vue3-client/src/http-common.js
+++ src/vue3-client/src/http-common.js
@@ -1,20 +1,20 @@
-import Axios from "axios";
+import Axios from 'axios';
 
 let axios = Axios.create({
-    //baseURL: "http://localhost:8083/api",
-    baseURL: "https://orga.symposion.hilton.rwth-aachen.de/api",
-    headers: {
-        "Content-type": "application/json",
-    }
+  //baseURL: "http://localhost:8083/api",
+  baseURL: 'https://orga.symposion.hilton.rwth-aachen.de/api',
+  headers: {
+    'Content-type': 'application/json',
+  },
 });
-axios.getFile = (fileId) => {
-    return axios.defaults.baseURL + "/file/" + fileId;
+axios.getFile = fileId => {
+  return axios.defaults.baseURL + '/file/' + fileId;
 };
-axios.getImage = (imageId) => {
-    return axios.getFile(imageId);
+axios.getImage = imageId => {
+  return axios.getFile(imageId);
 };
 axios.defaults.withCredentials = true;
-axios.defaults.baseWsURL = "ws://localhost:8083";
+axios.defaults.baseWsURL = 'ws://localhost:8083';
 // axios.defaults.baseWsURL = "wss://orga.symposion.hilton.rwth-aachen.de/scannerConsumer";
 
 export default axios;
--- src/vue3-client/src/main.js
+++ src/vue3-client/src/main.js
@@ -1,26 +1,26 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createBootstrap } from 'bootstrap-vue-next'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createBootstrap } from 'bootstrap-vue-next';
+import App from './App.vue';
+import router from './router';
 import { printDate, printDateTime, printDayDateTime } from './dateFilters';
 
 // Add the necessary CSS
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
 // Setup filters as global properties
 app.config.globalProperties.$filters = {
-    asDate: (value) => printDate(value),
-    asDateTime: (value) => printDateTime(value),
-    asDayDateTime: (value) => printDayDateTime(value),
-    asEuro: (n) => n === null ? "" : n.toLocaleString("de-DE", { maximumFractionDigits: 2 }) + " €"
-}
+  asDate: value => printDate(value),
+  asDateTime: value => printDateTime(value),
+  asDayDateTime: value => printDayDateTime(value),
+  asEuro: n => (n === null ? '' : n.toLocaleString('de-DE', { maximumFractionDigits: 2 }) + ' €'),
+};
 
-app.use(router)
-app.use(createBootstrap())
+app.use(router);
+app.use(createBootstrap());
 
-app.mount('#app')
+app.mount('#app');
