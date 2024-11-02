f39e211611f1527e1dab83a3429730e863ab7217
autoantwort <Leander.schulten@rwth-aachen.de>
2024-11-02 02:24:39 +0100

    Add dependencies for mqtt and icons

--- src/vue3-client/vite.config.js
+++ src/vue3-client/vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,12 +14,24 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     Components({
-      resolvers: [BootstrapVueNextResolver()],
+      resolvers: [
+        BootstrapVueNextResolver(),
+        IconsResolver({
+          alias: {
+            fa: 'fa6-solid',
+          }
+        }),
+      ],
+    }),
+    Icons({
+      defaultClass: 'icon-btn',
+      autoInstall: true,
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@common': fileURLToPath(new URL('../common', import.meta.url)),
     }
   }
 })
