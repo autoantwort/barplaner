1cc3024e24e3fe7703a43cd949809e2bde65c7af
autoantwort <Leander.schulten@rwth-aachen.de>
2024-11-02 19:48:37 +0100

    Use the hash history mode

--- src/vue3-client/src/router/index.js
+++ src/vue3-client/src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginView from '@/views/LoginView.vue';
 import axios from '@/http-common';
 import Roles from '@/roles';
 import Restricted from '@/views/restricted/Restricted.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
