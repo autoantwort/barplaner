d9d2b7f6d05ddd916b24e8e902f3ae508cde802c
autoantwort <Leander.schulten@rwth-aachen.de>
2025-11-27 22:31:46 +0100

    webNotificationsNewsletter.js: don't send newsletter every minute

--- src/Nodejs-RestAPIs/app/util/webNotificationsNewsletter.js
+++ src/Nodejs-RestAPIs/app/util/webNotificationsNewsletter.js
@@ -20,7 +20,7 @@ function getDayDiff(now, bar) {
     return (bar.start - now) / 1000 / 60 / 60 / 24;
 }
 
-const sendCronJob = new CronJob('00 * ' + env.webNotifications.sendTime + ' * * *', function() {
+const sendCronJob = new CronJob('00 00 ' + env.webNotifications.sendTime + ' * * *', function() {
     const now = new Date();
     Bar.findAll({
         where: {
